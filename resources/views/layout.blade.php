b2619360851a016701ef1e9a62814eb682c4d233
moosbeere <moosbeere_O@mail.ru>
2025-10-09 15:58:45 +0300

    Lab 12 env finish

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <title>Laravel</title>
 
@@ -70,6 +71,11 @@
         </header>
         <main>
           <div class="container mt-5">
+        
+<div id="app">
+
+ </div>
+
             @yield('content')
           </div>
         </main>
